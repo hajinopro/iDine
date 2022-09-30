7855d5ed169a99f16ecdc661c42c73f56b5ada0b
hajinopro <hajinopro@gmail.com>
2022-10-01 07:26:52 +0900

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by 하진호 on 2022/10/01.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
